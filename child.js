d28051e0e1b6019c30e5e5e5ebfbd331e7099761
sporeball <sporeballdev@gmail.com>
2020-12-01 21:27:21 -0800

    accept time as first argument

--- child.js
+++ child.js
@@ -10,11 +10,12 @@ const execa = require("execa");
 
 // arguments passed from parent
 var args = process.argv.slice(2);
-const _process = args[0];
-const file = args[1];
+const ms = args[0];
+const _process = args[1];
+const file = args[2];
 
 setTimeout(function() {
 	execa(_process, [file], {
 		detached: false
 	});
-}, 1000);
+}, ms);
--- index.js
+++ index.js
@@ -12,7 +12,14 @@ const execa = require("execa");
 
 var args = process.argv.slice(2);
 
-execa.node("child.js", [args[0], args[1]], {
+const time = args[0];
+const units = time.match(/\d+/g).map(i => +i).reverse()
+const ms = (units[0] * 1000) + (units[1] * 60000 || 0) + (units[2] * 3600000 || 0);
+
+const _process = args[1];
+const file = args[2];
+
+execa.node("child.js", [ms, _process, file], {
 	detached: true
 });
 
