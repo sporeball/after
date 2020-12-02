c203c42a5fb66d3d72b4282ede8b6d588be83b50
sporeball <sporeballdev@gmail.com>
2020-12-01 20:24:35 -0800

    first test

---
+++ child.js
@@ -0,0 +1,20 @@
+/*
+  child.js
+  in child process
+  copyright (c) 2020 sporeball
+  MIT license
+*/
+
+// dependencies
+const execa = require("execa");
+
+// arguments passed from parent
+var args = process.argv.slice(2);
+const _process = args[0];
+const file = args[1];
+
+setTimeout(function() {
+	execa(_process, [file], {
+		detached: false
+	});
+}, 1000);
---
+++ index.js
@@ -0,0 +1,19 @@
+#!/usr/bin/env node
+
+/*
+  index.js
+  in core
+  copyright (c) 2020 sporeball
+  MIT license
+*/
+
+// dependencies
+const execa = require("execa");
+
+var args = process.argv.slice(2);
+
+execa.node("child.js", [args[0], args[1]], {
+	detached: true
+});
+
+process.exit(0);
